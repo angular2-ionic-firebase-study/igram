fff92108b16c4fdbff44801b530ec025548bdc3f
이동욱 <jojoldu@gmail.com>
2017-03-19 17:00:00 +0900

    [board] displayName시 로그인 유형 관계없이 노출시키도록 수정 및 로그인 체크기능 추가

--- src/pages/board/board.ts
+++ src/pages/board/board.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
-import {ModalController} from 'ionic-angular';
+import {ModalController, NavController} from 'ionic-angular';
 import {AuthService} from "../../providers/auth-service";
+import {LoginPage} from "../login/login";
 import {BoardWritePage} from "./modal/board-write";
 
 import {AngularFire, FirebaseListObservable} from "angularfire2";
@@ -14,8 +15,15 @@ export class BoardPage {
 
   boards: FirebaseListObservable<any[]>;
 
-  constructor(public modalCtrl: ModalController, public af: AngularFire, private _auth: AuthService) {
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController, public af: AngularFire, private _auth: AuthService) {
     this.boards = af.database.list('boards');
+  }
+
+  ionViewWillEnter() {
+    if(!this._auth.authenticated) {
+      alert('로그인해야만 이용할 수 있습니다');
+      this.navCtrl.push(LoginPage);
+    }
   }
 
   showWriteModal() {
