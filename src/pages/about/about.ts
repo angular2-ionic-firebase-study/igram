af1125b3fac2c98066dbf3b947430152c47bca93
joshua1988 <jangkeehyo@gmail.com>
2017-03-21 15:11:21 +0900

    Moved the Camera Native functions to About page and added choose pic from Library buttons

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../providers/auth-service';
 
 import { FirebaseApp, AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Camera } from 'ionic-native';
 
 @Component({
   selector: 'page-about',
@@ -11,9 +12,16 @@ import { FirebaseApp, AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AboutPage {
 
+  public base64Image: any;
+  public guestPicture: any;
+
   public authorizedData: any;
   public storageRef: any;
-  public image: string;
+  public dbRef: any;
+
+  public uid: string;
+  public displayName: string;
+  private takenTime: string;
 
   public imgUrls: FirebaseListObservable<any[]>;
 
@@ -22,18 +30,80 @@ export class AboutPage {
 
     if (this.navParams.get('auth')) {
       this.authorizedData = this.navParams.get('auth').auth;
-      // console.log(this.authorizedData);
+      this.imgUrls = af.database.list('/imagesURLs');
     }
 
-    this.storageRef = firebaseApp.storage().ref().child('/images/new_sample.png');
-    this.storageRef.getDownloadURL().then(url => console.log(url));
+    this.storageRef = firebaseApp.storage().ref();
+    this.dbRef = af.database.list('/imagesURLs');
 
-    this.imgUrls = af.database.list('/imagesURLs');
+    this.uid = af.auth.getAuth().uid;
+    this.displayName = af.auth.getAuth().auth.displayName;
+
+    this.base64Image = null;
+    this.guestPicture = null;
+    this.takenTime = null;
+
+    // this.storageRef = firebaseApp.storage().ref().child('/images/new_sample.png');
+    // this.storageRef.getDownloadURL().then(url => console.log(url));
   }
 
   signOut() {
     this._auth.signOut();
     this.navCtrl.pop();
+  }
+
+  takePicture() {
+    return Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType : Camera.PictureSourceType.CAMERA,
+        encodingType: Camera.EncodingType.PNG,
+        targetWidth: 400,
+        targetHeight: 400
+    }).then((imageData) => {
+      this.base64Image = "data:image/png;base64," + imageData;
+      this.guestPicture = imageData;
+      this.takenTime = this.getDate();
+    }, (err) => {
+      alert(err);
+    });
+  }
+
+  selectFromPhotoAlbum() {
+    return Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+        encodingType: Camera.EncodingType.PNG,
+        targetWidth: 400,
+        targetHeight: 400
+    }).then((imageData) => {
+      this.base64Image = "data:image/png;base64," + imageData;
+      this.guestPicture = imageData;
+      this.takenTime = this.getDate();
+    }, (err) => {
+      alert(err);
+    });
+  }
+
+  uploadImage() {
+    return this.storageRef.child('/images/'+this.uid+'/new_sample.png')
+      .putString(this.guestPicture, 'base64', {contentType : 'image/png'})
+      .then((savedPicture) => {
+        this.dbRef.push({
+          "uid" : this.uid,
+          "name" : this.displayName,
+          "date" : this.takenTime,
+          "url" : savedPicture.downloadURL
+        }).then((success) => {
+          alert("URL uploaded : " + success);
+        }, (error) => {
+          alert("Failed URL upload" + error);
+        });
+      });
+  }
+
+  private getDate(): string {
+    var date = new Date();
+    return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
   }
 
 }
